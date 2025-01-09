873025e49f031612ddb0947566b0050b3f671277
Stefano Mazzolini <stefano.mazzolini@gmail.com>
2025-01-09 17:46:55 +0100

    post logic

--- chatbot.js
+++ chatbot.js
@@ -9,7 +9,17 @@ router.get('/', (req, res) => {
 });
 
 router.post('/', (req, res) => {
-    console.log('request', req);
+    
+    console.log('request headers', req.headers);
+    console.log('request body', req.body);
+
+    const data = req.body['data'];
+    const params = data['PARAMS'];
+    const message = params['MESSAGE'];
+
+    console.log('data', data);
+    console.log('params', params);
+    console.log('message', message);
 
     res.send('Ciao LCS POST !');
 });
